ae38e547fb27eabdfc8b78f0e4f1dc74189fdf5e
Jeff Inman <jeffery.inman@hotmail.com>
2021-01-23 17:03:41 -0800

    question five passed test

--- js/app.js
+++ js/app.js
@@ -113,11 +113,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  let arrayMultiplyOne = multiply(multArr[0], multArr[1])[0];
+  let arrayMultiplyTotal = multiply(arrayMultiplyOne, multArr[2])[0];
+  // console.log(arrayMultiplyOne);
+  // console.log(arrayMultiplyTotal);
+  let messageMultArray = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${arrayMultiplyTotal}.`;
+  return [arrayMultiplyTotal, messageMultArray];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
